47214bb9da11598b280cbae1364155caa43cf029
orezybsk <orezybsk@sample.co.jp>
2019-11-09 15:48:42 +0000

    権限管理のコードを実装しました

---
+++ aws/web-site-sample/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// IAM
+//
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    // principals の書き方は
+    // IAM JSON ポリシーエレメントのリファレンス
+    // https://docs.aws.amazon.com/ja_jp/IAM/latest/UserGuide/reference_policies_elements.html
+    // の中の「AWS JSON ポリシーの要素:Principal」参照
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example" {
+  role       = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
+
+module "describe_regions_for_ec2" {
+  source     = "./module/iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
