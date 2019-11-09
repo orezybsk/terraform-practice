f0473c43d34392a2ba06fc055943f97732ba2638
orezybsk <orezybsk@sample.co.jp>
2019-11-09 20:17:32 +0000

    ストレージのコードを実装しました

--- aws/web-site-sample/main.tf
+++ aws/web-site-sample/main.tf
@@ -49,3 +49,77 @@ module "describe_regions_for_ec2" {
   identifier = "ec2.amazonaws.com"
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// S3
+//
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform-orezybsk"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform-orezybsk"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://orezybsk.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform-orezybsk"
+
+  lifecycle_rule {
+    enabled = false
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    // 582318560864 は
+    // ap-northeast-1 の Elastic Load Balancing アカウント ID
+    // 以下のページ参照
+    // Classic Load Balancer のアクセスログの有効化
+    // https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/classic/enable-access-logs.html
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
